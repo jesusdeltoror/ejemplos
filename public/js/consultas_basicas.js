20d386bae840b70907055a55fd7002fe1d7fd194
Héctor Sánchez Tamayo <h_tamayo@yahoo.com>
2022-03-04 18:14:55 -0600

    Se agregó font-awesome para mostrar iconos de edición y borrado, actualización de estilos css

--- public/js/consultas_basicas.js
+++ public/js/consultas_basicas.js
@@ -1,16 +1,16 @@
 //Carga de Información de datos en formulario de edición
 function cargaEdit(info){
     console.log(info);
-    document.getElementById('nomEdit').value = info.nombre;
-    document.getElementById('edadEdit').value = info.edad;
-    document.getElementById('trabajoEdit').value = info.trabajo;
+    document.querySelector('#nomEdit').value = info.nombre;
+    document.querySelector('#edadEdit').value = info.edad;
+    document.querySelector('#trabajoEdit').value = info.trabajo;
     if(info.activo == "true"){
-        document.getElementById('activoEdit').checked = true;
+        document.querySelector('#activoEdit').checked = true;
     }
-    let controls = document.querySelector('#editForm').getElementsByTagName('input');
-    console.log(controls);
-    for(let i=0;i<controls.length;i++){
-        controls[i].disabled=false;
+    let controls = document.querySelector('#editForm')
+    let inputs = document.querySelectorAll('input');
+    for(let i=0;i<inputs.length;i++){
+        inputs[i].disabled=false;
     }
 }
 
